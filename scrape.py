f9c23d835738c997926c091e9b658f41ef718e32
Gurgen Verdian <gurgen.verdian@gmail.com>
2021-05-01 12:25:45 -0400

    more changes

--- scrape.py
+++ scrape.py
@@ -1,6 +1,20 @@
 import requests
-import bs4
+from bs4 import BeautifulSoup
 
 res = requests.get("https://news.ycombinator.com/")
+soup = BeautifulSoup(res.text, 'html.parser')
+links = soup.select('.storylink')
+subtext = soup.select('.subtext')
 
-print(res.text)
+def create_custom_hn(links, subtext):
+    hn = []
+    for idx, item in enumerate(links):
+        title = links[idx].getText()
+        href = links[idx].get('href', None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            hn.append({'title': title, 'link': href, 'votes': points})
+    return hn
+
+print(create_custom_hn(links, subtext))
